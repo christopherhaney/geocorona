aae436084b49bb8101d7aac1ec9c19993eed1da4
christopherhaney <chrishaney@u.boisestate.edu>
2020-09-22 15:07:31 -0600

    Centered loading icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'geocorona';
+  loaded = false;
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.loaded = true; // TODO set loaded after d3 has loaded, not just an arbitrary time
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapDetailContainerComponent } from './map-detail/map-detail-container/map-detail-container.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
 import { UsMapComponent } from './map-detail/us-map/us-map.component';
 import { UsStatePopUpComponent } from './map-detail/us-state-pop-up/us-state-pop-up.component';
 import { AboutComponent } from './about/about.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxCsvParserModule } from 'ngx-csv-parser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { NgxCsvParserModule } from 'ngx-csv-parser';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxCsvParserModule
+    NgxCsvParserModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
