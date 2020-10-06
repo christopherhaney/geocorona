318fe321ccf97684b6af8ffdac085be5738511c4
christopherhaney <chrishaney@u.boisestate.edu>
2020-10-06 15:30:17 -0600

    Changed travis node version

--- src/app/maps/usa-map/usa-map.component.spec.ts
+++ src/app/maps/usa-map/usa-map.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UsaMapComponent } from './usa.component';
+import { UsaMapComponent } from './usa-map.component';
 
 describe('UsaComponent', () => {
   let component: UsaMapComponent;
