779450259e7d3bbd36166a54cd10a4dd527c17df
christopherhaney <chrishaney@u.boisestate.edu>
2020-10-09 17:31:26 -0600

    Added navbar, broken label text color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs'; 
 import { AboutComponent } from './about/about.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxCsvParserModule } from 'ngx-csv-parser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +15,6 @@ import { UsaMapComponent } from './maps/usa-map/usa-map.component';
   declarations: [
     AppComponent,
     AboutComponent,
-    NavBarComponent,
     UsaMapComponent
   ],
   imports: [
@@ -24,7 +23,8 @@ import { UsaMapComponent } from './maps/usa-map/usa-map.component';
     HttpClientModule,
     NgxCsvParserModule,
     BrowserAnimationsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
