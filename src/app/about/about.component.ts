c6dc8db0ef4cb69f228e1b91f11dc061dead25ba
christopherhaney <43101437+christopherhaney@users.noreply.github.com>
2020-11-17 14:56:49 -0700

    Revert "About devs page"

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,35 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  benParagraph = "Benjamin Clark is a recent graduate with a Bachelor's in Computer Science from Boise State University. "
-  + "Ben currently works at Y STEM and Chess Inc as a front-end team lead intern. "
-  + "In his free time he likes to read books, skateboard, and exercise."
-
-  chrisParagraph = "Chris Haney is a recent graduate with a Bachelor's in Computer Science from Boise State University. "
-  + "He currently works at Micron Technology as a software engineer. "
-  + "He likes to DJ and play Super Smash Bros. Ultimate in his spare time!"
-
-  rossParagraph = "Ross McCusker is a recent graduate with a Bachelor's in Computer Science from Boise State University. " 
-  + "He currently works at HP as a software engineer and likes art, watching movies, and robots."
-
-  devList = [
-    {
-      img: 'ben',
-      paragraph: this.benParagraph
-    },
-    {
-      img: 'chris',
-      paragraph: this.chrisParagraph
-    },
-    {
-      img: 'ross',
-      paragraph: this.rossParagraph
-    }
-  ];
-
   constructor() { }
 
   ngOnInit(): void {
-
+    
   }
 }
