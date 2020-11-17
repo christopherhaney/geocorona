10e0356cfbde2105777bcf20d855fdb1a8663435
christopherhaney <chrishaney@u.boisestate.edu>
2020-11-17 14:19:29 -0700

    Formatting fixes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  benParagraph = "Benjamin Clark is a recent graduate with a Bachelor's in Computer Science from Boise State University."
-  + "Ben currently works at Y STEM and Chess Inc as a front-end team lead intern."
+  benParagraph = "Benjamin Clark is a recent graduate with a Bachelor's in Computer Science from Boise State University. "
+  + "Ben currently works at Y STEM and Chess Inc as a front-end team lead intern. "
   + "In his free time he likes to read books, skateboard, and exercise."
 
   chrisParagraph = "Chris Haney is a recent graduate with a Bachelor's in Computer Science from Boise State University. "
   + "He currently works at Micron Technology as a software engineer. "
   + "He likes to DJ and play Super Smash Bros. Ultimate in his spare time!"
 
-  rossParagraph = "Ross is also a recent Computer Science BS graduate from Boise State University. " +
+  rossParagraph = "Ross McCusker is a recent graduate with a Bachelor's in Computer Science from Boise State University. " +
                   "He currently works at HP as a software engineer and likes art, watching movies, and robots."
 
   devList = [
